02ea676b8a891b4178285b6ea2a0ffe4e9fd8884
Guido Castillo <guido.castillo.002@gmail.com>
2025-04-16 17:26:09 +0200

    Definiendo modelos del menu

--- ristorante_milano/menu/models.py
+++ ristorante_milano/menu/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+class Dish(models.Model):
+    category = models.ForeignKey(Category,
+                                 on_delete=models.RESTRICT,
+                                 related_name="dishes")
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Dishes"
