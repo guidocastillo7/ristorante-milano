5e923f3b1f8730ccc48ee87975c444268bdf5608
Guido Castillo <guido.castillo.002@gmail.com>
2025-04-16 16:55:14 +0200

    Creando apps de la web

--- ristorante_milano/ristorante_milano/settings.py
+++ ristorante_milano/ristorante_milano/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'menu',
+    'orders',
+    'users',
+    'web',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
