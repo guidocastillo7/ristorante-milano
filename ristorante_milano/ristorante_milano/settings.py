65192a9078c13b03de37bb075bf5d18e1505944e
Guido Castillo <guido.castillo.002@gmail.com>
2025-06-18 16:45:01 +0200

    Configurando la bbdd con postgresql y migrando los modelos del menu y usuarios

--- ristorante_milano/ristorante_milano/settings.py
+++ ristorante_milano/ristorante_milano/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0-a)k6c51jjz324lb$ewmn$ngj*2x99!&am5y+8!lb%!&uzf80'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,10 +79,7 @@ WSGI_APPLICATION = 'ristorante_milano.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(config("DATABASE_URL"))
 }
 
 
