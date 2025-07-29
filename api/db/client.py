2fbdfa23d16d1a404b2b3af104d05e6cc77aa0e5
Guido Castillo <guido.castillo.002@gmail.com>
2025-07-29 18:15:57 +0200

    Creando conexion a la bbdd

---
+++ api/db/client.py
@@ -0,0 +1,17 @@
+from fastapi import Depends
+from typing import Annotated, List
+from sqlmodel import Session, create_engine
+from decouple import config
+
+
+# Creating the connection to the database
+db_url = config("DATABASE_URL")
+engine = create_engine(db_url)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
