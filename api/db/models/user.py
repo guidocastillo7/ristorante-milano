814a40eff94aab4a3818ceaa4ed4db5698cefafd
Guido Castillo <guido.castillo.002@gmail.com>
2025-08-05 11:52:07 +0200

    Creando el modelo para User

---
+++ api/db/models/user.py
@@ -0,0 +1,8 @@
+from sqlmodel import SQLModel, Field
+
+
+# Model for the user table
+class User(SQLModel, table=True):
+    __tablename__ = "auth_user"
+    id: int = Field(primary_key=True)
+    username: str
