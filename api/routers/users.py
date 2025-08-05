8ddaafe451c0afdbe83103872f1388db7eb21d32
Guido Castillo <guido.castillo.002@gmail.com>
2025-08-05 11:52:43 +0200

    Creando el router para users

---
+++ api/routers/users.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter
+from sqlmodel import select
+from db.models.user import User
+from db.client import SessionDep
+from typing import List
+
+user_router = APIRouter(prefix="/users",
+                        tags=["users"])
+
+
+@user_router.get("/", response_model=List[User])
+async def get_users(
+    session: SessionDep,
+    offset: int = 0,
+    limit: int = 10
+):
+    users = session.exec(
+        select(User).offset(offset).limit(limit)
+    ).all()
+
+    return users
