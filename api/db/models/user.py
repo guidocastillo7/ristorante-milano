9449896a7ce008487d609114ec0e8078e5df5839
Guido Castillo <guido.castillo.002@gmail.com>
2025-08-19 19:26:01 +0200

    Agregando los campos faltantes al modelo de User

--- api/db/models/user.py
+++ api/db/models/user.py
@@ -1,12 +1,17 @@
 from sqlmodel import SQLModel, Field
+from datetime import datetime, UTC
 
 
 # Model for the user table
 class User(SQLModel, table=True):
     __tablename__ = "auth_user"
-    id: int = Field(primary_key=True)
+    id: int | None = Field(default=None, primary_key=True)
     username: str
     first_name: str
     last_name: str
     email: str
     password: str
+    is_superuser: bool = Field(default=False)
+    is_staff: bool = Field(default=False)
+    is_active: bool = Field(default=True)
+    date_joined: datetime = Field(default_factory=lambda: datetime.now(UTC))
