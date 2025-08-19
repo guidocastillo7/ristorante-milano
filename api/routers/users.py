b0f4577a339a39743c38fc5d5d3be88e0eb8573f
Guido Castillo <guido.castillo.002@gmail.com>
2025-08-19 17:58:30 +0200

    Cambiando el response_model para leer los users

--- api/routers/users.py
+++ api/routers/users.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from sqlmodel import select
 from db.models.user import User
+from db.schemas.user import UserRead
 from db.client import SessionDep
 from typing import List
 
@@ -8,7 +9,7 @@ user_router = APIRouter(prefix="/users",
                         tags=["users"])
 
 
-@user_router.get("/", response_model=List[User])
+@user_router.get("/", response_model=List[UserRead])
 async def get_users(
     session: SessionDep,
     offset: int = 0,
