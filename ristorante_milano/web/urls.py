b12bad093b8378abb20a4c2fd833c603106b16d0
Guido Castillo <guido.castillo.002@gmail.com>
2025-09-02 11:58:26 +0200

    Creando superusuario

--- ristorante_milano/web/urls.py
+++ ristorante_milano/web/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("create_superuser/", views.create_superuser),
 ]
--- ristorante_milano/web/views.py
+++ ristorante_milano/web/views.py
@@ -1,4 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from decouple import config
 
 def home(request):
     return render(request, "home.html")
+
+
+def create_superuser(request):
+    username = "admin"
+    password = "123456789"
+
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username,
+                                      password=password)
+
+    return redirect("/")
