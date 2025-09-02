93a12499c901288865ba2f56541cc4931f391861
Guido Castillo <guido.castillo.002@gmail.com>
2025-09-02 11:13:50 +0200

    Arreglando el build

--- ristorante_milano/build.sh
+++ ristorante_milano/build.sh
@@ -2,6 +2,8 @@
 # exit on error
 set -o errexit
 
+cd ..
+pip install --upgrade pip
 pip install -r ./requirements.txt
 python ristorante_milano/manage.py collectstatic --no-input
 python ristorante_milano/manage.py migrate
