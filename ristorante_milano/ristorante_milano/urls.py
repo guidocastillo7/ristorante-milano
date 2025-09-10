02f9f0ad951995671fd365dba9804e4125606081
Guido Castillo <guido.castillo.002@gmail.com>
2025-09-10 11:36:29 +0200

    Probando ajuste para deploy

--- ristorante_milano/ristorante_milano/urls.py
+++ ristorante_milano/ristorante_milano/urls.py
@@ -24,5 +24,3 @@ urlpatterns = [
     path("menu/", include("menu.urls")),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
