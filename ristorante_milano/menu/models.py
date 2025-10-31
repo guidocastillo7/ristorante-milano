e3e73d47b2ac5fe046bc913dec6d3ebb51d4ab48
Guido Castillo <guido.castillo.002@gmail.com>
2025-10-31 10:23:08 +0100

    Cambiando a opcional el campo description de dishes

--- ristorante_milano/menu/models.py
+++ ristorante_milano/menu/models.py
@@ -17,7 +17,7 @@ class Dish(models.Model):
                                  on_delete=models.RESTRICT,
                                  related_name="dishes")
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     available = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
